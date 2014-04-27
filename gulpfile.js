e031dacb1895a4c998ba6f6d6e5e963a677017f6
robert <robert@robertjd.com>
2014-04-27 14:20:11 -0700

    use less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var gulp = require('gulp'),
-  clean = require('gulp-clean');
+  clean = require('gulp-clean'),
+  less = require('gulp-less');
 
 var jade = require('gulp-jade');
 
@@ -16,17 +17,24 @@ gulp.task('jade', function(){
     .pipe(gulp.dest('.tmp/'));
 });
 
+gulp.task('less', function () {
+  gulp.src('main.less')
+    .pipe(less({}))
+    .pipe(gulp.dest('.tmp/'));
+});
+
 gulp.task('clean', function() {
   return gulp.src(['.tmp/'], {read: false})
     .pipe(clean());
 });
 
-gulp.task('watch',function(){
+gulp.task('watch',['clean'],function(){
   gulp.watch('*.jade',['jade']);
+  gulp.watch('*.less',['less']);
 });
 
 //TODO
-// stylus
+
 // livereload
 // js hint
 // jsmin
