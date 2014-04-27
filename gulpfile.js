975da689aae3aa5a8216168074cbf72f0415213f
robert <robert@robertjd.com>
2014-04-27 10:53:29 -0700

    clean task, only build index

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  clean = require('gulp-clean');
 
 var jade = require('gulp-jade');
 
@@ -8,14 +9,24 @@ gulp.task('jade', function(){
 
   var YOUR_LOCALS = {};
 
-  gulp.src('*.jade')
+  gulp.src('index.jade')
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest('./.tmp/'))
+    .pipe(gulp.dest('.tmp/'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src(['.tmp/'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('watch',function(){
+  gulp.watch('*.jade',['jade']);
 });
 
 //TODO
+// stylus
 // livereload
 // js hint
 // jsmin
